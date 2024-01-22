194b710327e3bdcae35041dd3915267a1b8586a8
JonasRettig <jrettig@uni-muenster.de>
2024-01-22 16:39:15 +0100

    fixed mobile flaws

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -4,16 +4,16 @@
 export default function Header() {
     return (
         <div className="w-screen flex" id="header">
-            <div className="w-4/6 m-3 flex justify-between mx-auto">
+            <div className="w-4/6 m-3 flex justify-between mx-auto max-sm:w-11/12">
                 <a href="/" id="header">
                     Home
                 </a>
-                <div className="w-2/6 flex justify-around">
+                <div className="w-2/6 flex justify-around max-sm:w-3/6">
                     <a href="/skills" id="header">
                         Skills
                     </a>
                     <a href="/projects" id="header">
-                        Projects
+                        Projekte
                     </a>
                     <a href="/about" id="header">
                         About
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -38,22 +38,22 @@ export default function Projects() {
     return (
         <div>
             <Header />
-            <div className="w-4/6 flex justify-self-center mx-auto">
+            <div className="w-4/6 flex justify-self-center mx-auto max-sm:w-11/12">
                 <div className="flex flex-col">
                     <h1 className="font-bold"> Meine Projekte </h1>
                     <div>
                         {projectsArray.map((project) => (
-                            <div className="flex flex-col border-t-2 m-2">
+                            <div className="flex flex-col border-t-2 m-2 max-sm:m-0">
                                 <div className="flex-col mt-1">
                                     <div className="flex flex-row">
                                         <h2 className="font-bold mr-2"> {project.name} </h2>
                                         <div> {project.timeframe} </div>
                                     </div>
-                                    <div className="flex">
-                                        <div className="mt-2 mr-2 w-3/6">
+                                    <div className="flex max-sm:flex-col">
+                                        <div className="mt-2 mr-2 w-3/6 max-sm:w-full">
                                             <img src={`/${project.image}`} alt={project.imageAlt} className="object-contain" />
                                         </div>
-                                        <div className="mt-1 flex w-2/6 content-center">
+                                        <div className="mt-1 flex w-2/6 content-center max-sm:w-full">
                                             {project.description}
                                         </div>
                                     </div>
@@ -65,28 +65,30 @@ export default function Projects() {
                                             </div>
                                         ))}
                                     </div>
-                                    <div className="flex flex-row">
+                                    <div className="flex flex-row max-sm:mb-3">
                                         <div className="mt-2 mr-2">
                                             <b> Genutzte Technologien: </b>
-                                            <div className="flex flex-row">
+                                            <div className="flex flex-row max-sm:flex-col" >
                                                 {project.technologies.map((technology) => (
-                                                    <div className="mr-2">
+                                                    <div className="mr-2 text">
                                                         {technology}
                                                     </div>
                                                 ))}
                                             </div>
                                         </div>
                                         {project.links && (
-                                            <div className="mt-2">
-                                                <b> Links: </b>
-                                                <div className="flex flex-row">
-                                                    {project.links.map((link) => (
-                                                        <div className="mr-2">
-                                                            <a href={link.url} target="_blank">
-                                                                {link.name}
-                                                            </a>
-                                                        </div>
-                                                    ))}
+                                            <div className="mt-2 max-sm:border-l">
+                                                <div className="max-sm:ml-3">
+                                                    <b> Links: </b>
+                                                    <div className="flex flex-row max-sm:flex-col">
+                                                        {project.links.map((link) => (
+                                                            <div className="mr-2">
+                                                                <a href={link.url} target="_blank">
+                                                                    {link.name}
+                                                                </a>
+                                                            </div>
+                                                        ))}
+                                                    </div>
                                                 </div>
                                             </div>
                                         )}
