6732e418a8551a6fabb6e383cc346122cdade416
JonasRettig <jrettig@uni-muenster.de>
2024-01-22 15:27:35 +0100

    first work on projects site

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,18 +1,20 @@
 
 export default function Header() {
     return (
-        <div className="m-3 flex justify-between">
-            <button>Home</button>
-            <div className="w-1/6 flex justify-around">
-                <a href="/skills">
-                    Skills
-                </a>
-                <a href="/projects">
-                    Projects
-                </a>
-                <a href="/about">
-                    About
-                </a>
+        <div className="w-screen flex">
+            <div className="w-4/6 m-3 flex justify-between mx-auto">
+                <button>Home</button>
+                <div className="w-2/6 flex justify-around">
+                    <a href="/skills">
+                        Skills
+                    </a>
+                    <a href="/projects">
+                        Projects
+                    </a>
+                    <a href="/about">
+                        About
+                    </a>
+                </div>
             </div>
         </div>
     )
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,10 +1,93 @@
+'use client'
 import Header from "@/Components/header";
+import projects from "@/data/projects.json";
+import { useEffect, useState } from "react";
 
 export default function Projects() {
+
+    interface Project {
+        id: number,
+        name: string,
+        description: string[],
+        technologies: string[],
+        myWork: string[],
+        links?: link[]
+    }
+
+    interface link {
+        url: string,
+        name: string
+    }
+
+    useEffect(() => {
+        getProjects();
+    }, [])
+
+    const [projectsArray, setProjectsArray] = useState<Project[]>([]);
+
+    function getProjects() {
+        var projectsArray:Project[] = Object.values(projects);
+        projectsArray.sort((a, b) => (a.id > b.id) ? 1 : -1)
+        setProjectsArray(projectsArray);
+    }
+
     return (
         <div>
             <Header/>
-            My Projects
+            <div className="w-4/6 flex justify-self-center mx-auto">
+                <div className="flex flex-col">
+                    <h1 className="font-bold"> Meine Projekte </h1>
+                    <div>
+                        {projectsArray.map((project) => (
+                            <div className="flex flex-col border-t-2 m-2">
+                                <div className="flex-col">
+                                    <h2 className="font-bold"> {project.name} </h2>
+                                    <div className="mt-1">
+                                        <b> Beschreibung: </b>
+                                        {project.description.map((description) => (
+                                            <div>
+                                                {description}
+                                            </div>
+                                        ))}
+                                    </div>
+                                    <div className="mt-2">
+                                        <b> Meine Arbeit: </b>
+                                        {project.myWork.map((myWork) => (
+                                            <div>
+                                                {myWork}
+                                            </div>
+                                        ))}
+                                    </div>
+                                    <div className="mt-2">
+                                        <b> Genutzte Technologien: </b>
+                                        <div className="flex flex-row">
+                                            {project.technologies.map((technology) => (
+                                                <div className="mr-2">
+                                                    {technology}
+                                                </div>
+                                            ))}
+                                        </div>
+                                    </div>
+                                    {project.links && (
+                                        <div className="mt-2">
+                                            <b> Links: </b>
+                                            <div className="flex flex-row">
+                                                {project.links.map((link) => (
+                                                    <div className="mr-2">
+                                                        <a href={link.url} target="_blank">
+                                                            {link.name}
+                                                        </a>
+                                                    </div>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div> 
         </div>
     )
 }
