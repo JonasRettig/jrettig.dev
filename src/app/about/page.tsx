cfb6c5390513103c695b81f997eeb761f687387f
JonasRettig <jrettig@uni-muenster.de>
2024-01-22 17:26:00 +0100

    added first text for about and skills

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,7 +4,18 @@ export default function About() {
     return (
         <div>
             <Header/>
-            About me
+            <div className="w-4/6 flex flex-col justify-self-center mx-auto max-sm:w-11/12">
+                <div>
+                    Ich bin Jonas, gebürtiger Münsteraner. Die letzten Jahre habe ich an der Universität Münster Wirtschaftsinformatik studiert und bin nun auf der Suche nach einer neuen Herausforderung.
+                </div>
+                <div>
+                    Im Studium habe ich gelernt, dass ich mich sehr für die Entwicklung von Software interessiere. Besonders spannend finde ich dabei die Frontend-Entwicklung, da ich hier meine kreative Ader ausleben kann.
+                </div>
+                <div>
+                    Mein ganzer Lebenslauf ist wie folgt:
+                </div>
+                <embed src="Lebenslauf.pdf" className="w-4/6 h-screen" />
+            </div>
         </div>
     )
 }
--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -4,17 +4,34 @@ export default function Skills() {
     return (
         <div>
             <Header/>
-            <div>
-                Webentwicklung
-            </div>
-            <div>
-                Sonstige Entwicklung
-            </div>
-            <div>
-                Methodik
-            </div>
-            <div>
-                Soft Skills
+            <div className="w-4/6 flex flex-col justify-self-center mx-auto max-sm:w-11/12">
+                <div className="mb-2">
+                    <b> Webentwicklung </b>
+                    <div>
+                    In den letzten Jahren habe ich mich hauptsächlich mit Webentwicklung beschäftigt, wobei ich mich auf Frontend-Entwicklung spezialisiert habe. 
+                    Meine Arbeit lag dabei hauptsächlich in der Anwendungsentwicklung, mittlerweile durfte ich aber auch Erfahrung im Design mit Tailwind CSS sammeln.
+                    </div>
+                </div>
+                <div>
+                    <b> Sonstige Entwicklung </b>
+                    <div>
+                    Durch mein Studium der Wirtschaftsinformatik habe ich auch Erfahrung in anderen Bereichen der Softwareentwicklung gesammelt. So habe ich bereits mit Java, Python, R, Haskell und SQL gearbeitet. 
+                    Zudem bin ich mit verschiedenen Softwaretools, welche in der Softwareentwicklung eingesetzt werden, vertraut. Hierzu zählen unter anderem Git, Docker und Jira.
+                    </div>
+                </div>
+                <div>
+                    <b> Methodik </b>
+                    <div>
+                    Zudem konnte ich durch mein Studium Kontakt mit verschiedenen Methoden der Softwareentwicklung sammeln. 
+                    Besonders spannend finde ich hier Design Thinking, zu welchem ich auch bei einem ERASMUS+ Vertiefungsmodul in den USA viel lernen konnte.
+                    </div>
+                </div>
+                <div>
+                    <b> Soft Skills </b>
+                    <div>
+                    Durch meine Erfahrung beim Sozialkompass konnte ich meine Soft Skills weiterentwickeln. Ich kann sehr gut im Team arbeiten und bin sehr kommunikativ. Zudem bin ich sehr lernbereit und kann mich schnell in neue Themen einarbeiten.
+                    </div>
+                </div>
             </div>
         </div>
     )
