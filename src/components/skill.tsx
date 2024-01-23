264b1ff522da6440223270741f0b8bf9fdc9933c
JonasRettig <jrettig@uni-muenster.de>
2024-01-23 20:41:03 +0100

    removed ability skills

--- src/components/skill.tsx
+++ src/components/skill.tsx
@@ -2,12 +2,8 @@
 export default function Skill({ icon, skill, info, level }: { icon: any, skill: string, info: string, level: number }) {
     return (
         <div className="flex flex-row my-5 items-center">
-            <div className="text-9xl mr-2 flex flex-col">
+            <div className="text-8xl mr-4 flex flex-col">
                 {icon}
-                <div className="bg-black rounded-3xl h-[20px] w-full mt-2" >
-                    <div style={{ width: `${(level / (10)) * 100}%` }} className=" bg-white rounded-3xl h-full">
-                    </div>
-                </div>
             </div>
             <div className="mb-2">
                 <b className="text-lg"> {skill} </b>
